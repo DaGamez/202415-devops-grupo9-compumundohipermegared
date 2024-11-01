5d14c88f21ef47deae837f3cfdcb3235f98b53a9
Daniel Gamez <dgamez@porvenir.com.co>
2024-11-01 18:00:40 -0500

    ajuste code build project, funciona generando .txt

--- aws-cli.sh
+++ aws-cli.sh
@@ -25,6 +25,14 @@ export AWS_PROFILE=devops-compumundo
 #list iam users
 aws iam list-users --output table
 
+#se crea bucket de s3 para almacenar artefactos
+aws s3api create-bucket --bucket compumundo-black-list-s3 --region us-west-2 --create-bucket-configuration LocationConstraint=us-west-2
+
+#se listan los buckets de s3
+aws s3api list-buckets --output table
+
+#se elimina bucket de s3 (al final de la sesion de uso)
+aws s3api delete-bucket --bucket compumundo-black-list-s3 --region us-west-2
 
 #create code build project
 # idea es que construya una imagen de contenedor en un S3
@@ -32,6 +40,12 @@ aws codebuild create-project --cli-input-json file://create-aws-code-build-proje
 
 #listar codebuild projects
 aws codebuild list-projects --output table
+
+#comenzar build del proyecto
+aws codebuild start-build --project-name "compumundo-black-list-CI"
+
+#build status con id del build del anterior paso
+aws codebuild batch-get-builds --ids "compumundo-black-list-CI:<id>"
 
 #delete codebuild project
 aws codebuild delete-project --name "compumundo-black-list-CI"
