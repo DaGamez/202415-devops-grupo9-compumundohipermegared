28c6028bd64e2be2fe409a97eb6e7fd81b26476b
Daniel Gamez <dgamez@porvenir.com.co>
2024-11-13 18:38:52 -0500

    se agregan target groups

---
+++ terraform/target_groups.tf
@@ -0,0 +1,47 @@
+resource "aws_lb_target_group" "target_group_1" {
+  name        = "target-group-1"
+  port        = 5000
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 3
+    interval           = 30
+    matcher            = "200"
+    path               = "/"
+    port               = "traffic-port"
+    protocol           = "HTTP"
+    timeout            = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "Target Group 1"
+  }
+}
+
+resource "aws_lb_target_group" "target_group_2" {
+  name        = "target-group-2"
+  port        = 5000
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 3
+    interval           = 30
+    matcher            = "200"
+    path               = "/"
+    port               = "traffic-port"
+    protocol           = "HTTP"
+    timeout            = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "Target Group 2"
+  }
+}
